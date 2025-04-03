ce4b72e12d9e736e0cff42d4b669ff2ba236a670
HadeerAlaa542 <hadeeralaa542@gmail.com>
2025-04-03 03:19:35 +0200

    Update pushManifests.groovy

--- vars/pushManifests.groovy
+++ vars/pushManifests.groovy
@@ -1,4 +1,6 @@
 def call() {
+    git config --global user.email "hadeeralaa542@gmail.con"
+    git config --global user.name "hadeer"
     sh 'git add .'
     sh 'git commit -m "push Manifests" '
     sh 'git push'
